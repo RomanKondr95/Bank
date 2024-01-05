276f3f8ec52c39e7e2ba5b764efa6830509607d0
RomanKondr95 <139111155+RomanKondr95@users.noreply.github.com>
2024-01-05 19:46:32 +0300

    Added model Transactions

--- bank/wallets/admin.py
+++ bank/wallets/admin.py
@@ -1,5 +1,6 @@
+from decimal import Decimal
 from django.contrib import admin
-from .models import Wallets
+from .models import Wallets, Transactions
 
 
 @admin.register(Wallets)
@@ -15,5 +16,27 @@ class WalletAdmin(admin.ModelAdmin):
                     obj.balance = 100
             if not obj.name:
                 obj.name = obj.generate_unique_name()
+
+        super().save_model(request, obj, form, change)
+
+
+# admin.site.register(Transactions)
+
+
+@admin.register(Transactions)
+class TransactionAdmin(admin.ModelAdmin):
+    fields = ("sender", "receiver", "transfer_amount")
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            if obj.sender.currency != obj.receiver.currency:
+                obj.status = "FAILED"
+                obj.save()
+                return
+
+            if obj.sender.user != obj.receiver.user:
+                obj.commission = obj.transfer_amount * Decimal(0.10)
+            else:
+                obj.commission = Decimal(0.00)
 
         super().save_model(request, obj, form, change)
--- bank/wallets/models.py
+++ bank/wallets/models.py
@@ -1,8 +1,10 @@
+from decimal import Decimal
 import secrets
 import string
 from django.db import models
 from django.contrib.auth.models import User
 from django.forms import ValidationError
+from django.core.validators import MinValueValidator
 
 
 class Wallets(models.Model):
@@ -37,13 +39,36 @@ class Wallets(models.Model):
         user_wallets_count = Wallets.objects.filter(user=self.user).count()
         if not self.name:
             self.name = self.generate_unique_name()
-        if not self.pk:
-            if not self.balance:
-                if self.currency in [self.USD, self.EUR]:
-                    self.balance = 3
-                elif self.currency == self.RUB:
-                    self.balance = 100
-            if user_wallets_count >= 5:
-                raise ValidationError("User cannot create more than 5 wallets")
+        if not self.balance:
+            if self.currency in [self.USD, self.EUR]:
+                self.balance = 3
+            elif self.currency == self.RUB:
+                self.balance = 100
+        if user_wallets_count >= 5:
+            raise ValidationError("User cannot create more than 5 wallets")
         super().save(*args, **kwargs)
 
+
+class Transactions(models.Model):
+    sender = models.ForeignKey(
+        Wallets, related_name="sent_transaction", on_delete=models.CASCADE
+    )
+    receiver = models.ForeignKey(
+        Wallets, related_name="received_transaction", on_delete=models.CASCADE
+    )
+    transfer_amount = models.DecimalField(
+        max_digits=10, decimal_places=2, validators=[MinValueValidator(0)]
+    )
+    comission = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    status = models.CharField(max_length=10, default="PAID")
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def save(self, *args, **kwargs):
+        if self.sender.currency != self.sender.currency:
+            self.status = 'FAILED'
+            raise ValidationError("Transaction allowed only for wallets with the same currency!")
+        if self.sender.user != self.receiver.user:
+            self.comission = self.transfer_amount * Decimal(0.10)
+        else:
+            self.commission = Decimal(0.00)
+        super().save(*args, **kwargs)
